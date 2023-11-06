4ace9dd49f532746020e21d13401dd76cb314d92
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2023-11-06 16:09:22 +0300

    Restaurant-Schema and Crud Done!

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './schemas/restaurant.schema';
+import { CreateRestaurantDto, UpdateRestaurantDto } from './dtos/index';
+
 
 @Controller('restaurants')
-export class RestaurantsController {}
+export class RestaurantsController {
+    constructor(
+        private restauranstService: RestaurantsService
+    ){}
+
+    @Get()
+    async getAllRestaurants():Promise<Restaurant[]>{
+        return this.restauranstService.findAll();
+    }
+
+    @Post()
+    async createRestaurants(@Body() restaurant:CreateRestaurantDto): Promise<Restaurant>{
+        return this.restauranstService.create(restaurant)
+    }
+
+    @Get(':id')
+    async getRestaurant(@Param('id') id: string): Promise<Restaurant>{
+        return this.restauranstService.findById(id);
+    }
+
+    @Put(':id')
+    async updateRestaurant(@Param('id') id: string, @Body() restaurant: UpdateRestaurantDto): Promise<Restaurant>{
+        await this.restauranstService.findById(id);
+        return this.restauranstService.updateById(id, restaurant);
+    }
+
+    @Delete(':id')
+    async deleteRestaurant(@Param('id') id: string): Promise<Restaurant>{
+        await this.restauranstService.findById(id);
+        return this.restauranstService.deleteById(id)
+    }
+}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Restaurant } from './schemas/restaurant.schema';
+import { CreateRestaurantDto, UpdateRestaurantDto } from './dtos/index';
 
 @Injectable()
-export class RestaurantsService {}
+export class RestaurantsService {
+    constructor(
+        @InjectModel(Restaurant.name)
+        private restaurantModel: mongoose.Model<Restaurant>
+    ){}
+
+    async findAll(): Promise<Restaurant[]>{
+        const restaurants = await this.restaurantModel.find({});
+        return restaurants;
+    };
+
+    async create(restaurant:CreateRestaurantDto): Promise<Restaurant>{
+        const newRestaurant = await this.restaurantModel.create(restaurant)
+        return newRestaurant;
+    };
+
+    async findById(id: string): Promise<Restaurant>{
+        const restaurant = await this.restaurantModel.findById(id);
+
+        if(!restaurant){
+            throw new NotFoundException('Restaurant not found');
+        }
+        
+        return restaurant;
+    };
+
+
+    async updateById(id: string, restaurant: UpdateRestaurantDto): Promise<Restaurant>{
+        return await this.restaurantModel.findByIdAndUpdate(id,restaurant,{
+            new: true,
+            runValidators: true,
+        }); 
+    };
+
+    async deleteById(id:string): Promise<Restaurant>{
+        return await this.restaurantModel.findByIdAndRemove(id);
+    }
+}
